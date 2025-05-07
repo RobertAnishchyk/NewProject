202663862f591963e46184e6b82c0c1a27130df5
RobertAnishchyk <robertanisik@gmail.com>
2025-05-08 02:51:05 +0300

    задание 2

---
+++ src/main/java/Task.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task {
+
+
+    public static void main (String[] args ){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ширина: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Длина: ");
+        int num2 = scanner.nextInt();
+        System.out.print("Площадь: " + (num1 * num2));
+
+    }
+}
