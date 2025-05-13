0c056669e6c6559ed92900f51684be9a1c65330e
RobertAnishchyk <robertanisik@gmail.com>
2025-05-13 03:02:08 +0300

    полиндром

---
+++ src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/*
+Задание1
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int orig = number;
+        int polindrom = 0;
+
+        while (number > 0) {
+            polindrom = polindrom * 10 + number % 10;
+            number /= 10;
+        }
+        if (orig == polindrom) {
+            System.out.println("Полиндром");
+        }
+        else {
+            System.out.println("Не Полиндром");
+
+        }
+
+
+/*Задание2
+
+*/
+    }
+}
